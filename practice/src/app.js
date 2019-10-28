48f3837da525b9e551d4f125610994150f89378e
Ian Smith <ianmsmith17@gmail.com>
2019-10-28 13:26:32 -0500

    initialized repo

---
+++ practice/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useEffect, useState, useReducer } from 'react';
+
+export default function App() {
+  return (
+    <>
+      <div>My App</div>
+    </>
+  );
+}
